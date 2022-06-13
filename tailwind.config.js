afd3d613a1d7bd2b41453a54cdf51eb0eabdb851
MarijaPaunovic <marijapaunovicdesign@gmail.com>
2022-06-13 18:32:37 +0200

    first init

---
+++ tailwind.config.js
@@ -0,0 +1,27 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './dist/index.html'
+  ],
+  theme: {
+    extend: {
+      screens: {
+        xs: '375px',
+        sm: '480px',
+        md: '768px',
+        lg: '976px',
+        xl: '1440px',
+      },
+      colors: {
+        primary: {
+          dark: 'hsl(179, 62%, 43%)', //	#2ab2af - dark green
+          light: 'hsl(179, 47%, 52%)' // #4abebd - light green 
+        },
+        secondary: 'hsl(71, 73%, 54%)', // #c0df34 - yellow
+        lightGray: 'hsl(204, 43%, 93%)', // #e5eff5 - background
+        grayishBlue: 'hsl(218, 22%, 67%)' // #98a6bd - text color
+      },
+    },
+  },
+  plugins: [],
+}
